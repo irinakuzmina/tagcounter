4a058e5af8245c1c90cb48cd2996172b24c29946
irinakuzmina <irinakuzmina@tut.by>
2020-11-21 23:39:14 +0300

    fix

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 This is a simple example package. You can use
 [Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
 to write your content.
+
+main.py --get https://www.tut.by/
+main.py --view https://www.tut.by/
+
+main.py
+
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="tagcounter", # Replace with your own username
+    name="tagcounter",
     version="0.0.1",
     author="Iryna Kuzmina",
     author_email="iryna_kuzmina@epam.com",
     description="Html Tags Counter package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/irinakuzmina/tagcounter.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
